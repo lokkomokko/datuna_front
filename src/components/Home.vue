46bbce60eb073de05412f2e06e6b108a1e26b212
lokkomokko <lokkomokko1@gmail.com>
2017-09-14 15:15:34 +0300

    start project

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h1>HOME PAGE</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js PWA'
+    }
+  }
+}
+</script>
+
